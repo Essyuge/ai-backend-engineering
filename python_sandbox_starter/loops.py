cf2b6624faa1254afa600ca533801d86fa58a9b9
Essyuge <ugeesther@gmail.com>
2026-05-09 16:45:37 +0300

    add loops.py and modules.py

--- python_sandbox_starter/loops.py
+++ python_sandbox_starter/loops.py
@@ -1,5 +1,54 @@
-# A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string).
+# A for loop is used for iterating over a sequence
+#  (that is either a list,
+#  a tuple,
+#  a dictionary,
+#  a set,
+#  or a string).
+people = ['John', 'Jane', 'Doe']
+for person in people:
+    print(person)  # John, Jane, Doe (each name on a new line)
 
+# Breaking out of a loop
+for person in people:
+    if person == 'Jane':
+        break  # This will stop the loop when it reaches 'Jane'
+    print(person)  # John (only John will be printed)
 
+    # continuing to the next iteration
+    for person in people:
+        if person == 'Jane':
+            continue  # This will skip the rest of the loop when it reaches 'Jane' and continue with the next iteration
+        print(person)  # John, Doe (Jane is skipped)
+
+# Range function - The range() function returns a sequence of numbers, starting from 0 by default, and increments by 1 (by default), and ends at a specified number.
+for i in range(5):
+    print(i)  # 0, 1, 2, 3, 4 (each number on a new line)
+
+for i in range(len(people)):
+    print(people[i])  # John, Jane, Doe (each name on a new line)
+
+for i in range(1, 10, 2):
+    print(i)  # 1, 3, 5, 7, 9 (each number on a new line)
+
+for i in range(10, 0, -1):
+    print(i)  # 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 (each number on a new line)
+
+for i in range(5):
+    for j in range(3):
+        print(f'i: {i}, j: {j}')  # Nested loop example (prints the values of i and j)
 
 # While loops execute a set of statements as long as a condition is true.
+count = 0
+while count<= 10:
+    print(f'count: {count}')  # 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 (each number on a new line)
+    count += 1
+
+while True:
+    response = input('Enter a number (or "exit" to quit): ')
+    if response.lower() == 'exit' or response.lower() == 'quit':
+        break  # This will exit the loop if the user types 'exit' or 'quit'
+    try:
+        number = int(response)
+        print(f'You entered: {number}')  # This will print the number entered by the user
+    except ValueError:
+        print('Please enter a valid number or "exit" to quit.')  # This will handle the case where the user enters something that is not a number
--- python_sandbox_starter/modules.py
+++ python_sandbox_starter/modules.py
@@ -1,1 +1,23 @@
-# A module is basically a file containing a set of functions to include in your application. There are core python modules, modules you can install using the pip package manager (including Django) as well as custom modules
+# A module is basically a file containing a set of functions to include in your application. 
+# There are core python modules, 
+# modules you can install using the pip package manager (including Django) as well as custom modules
+# core python modules are built into python and can be used without installing anything
+import math  # Importing the math module
+import datetime  # Importing the datetime module
+from datetime import date, time  # Importing the date class from the datetime module
+import random  # Importing the random module
+print(math.sqrt(16))  # 4.0 (using the sqrt function from the math module)
+
+print(random.randint(1, 10))  # Random integer between 1 and 10
+
+
+print(date.today() - datetime.timedelta(days=10))  # 10 days ago
+print(time())  # Current time in seconds since the epoch
+print(datetime.datetime.now())  # Current date and time
+print(datetime.date.today())  # Current date
+
+# pip module example
+import camelcase  # Importing the camelcase module (you need to install it using pip)
+c = camelcase.CamelCase()  # Creating an instance of the CamelCase class
+print(c.hump('hello world'))  # Converting a string to CamelCase
+
