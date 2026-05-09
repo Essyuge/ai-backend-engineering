ddce3acb2064c5f72c2ebb47b6029573ec623243
Essyuge <ugeesther@gmail.com>
2026-05-09 16:52:24 +0300

    add modules.py

--- python_sandbox_starter/modules.py
+++ python_sandbox_starter/modules.py
@@ -16,8 +16,18 @@ print(time())  # Current time in seconds since the epoch
 print(datetime.datetime.now())  # Current date and time
 print(datetime.date.today())  # Current date
 
-# pip module example
-import camelcase  # Importing the camelcase module (you need to install it using pip)
-c = camelcase.CamelCase()  # Creating an instance of the CamelCase class
-print(c.hump('hello world'))  # Converting a string to CamelCase
+# # pip module example
+# import camelcase  # Importing the camelcase module (you need to install it using pip)
+# c = camelcase.CamelCase()  # Creating an instance of the CamelCase class
+# print(c.hump('hello world'))  # Converting a string to CamelCase
 
+# import custom module example
+import validator  # Importing the custom validator module
+from validator import validate_email  # Importing the validate_email function from the validator module
+print(validate_email('example@example.com'))  # Validating an email address
+
+email = 'example@example.com'
+if  validate_email(email):
+    print('Valid email')
+else:
+    print('Invalid email')
